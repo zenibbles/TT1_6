4a298d013e30fbab58d6d81964fde42b6c7ac283
lesterbek <lesterbek@gmail.com>
2021-09-30 14:07:57 +0800

    cart

--- frontend/src/components/Cart.js
+++ frontend/src/components/Cart.js
@@ -5,19 +5,35 @@ export default function Cart(){
 
     return(
         <html>
+            <head>
+                <style>
+                    
+                </style>
+            </head>
+            
+            <NavbarComponent/>
         <h1>Cart Page</h1>
         <table>
             <tr>
-            <th>Product</th>
-            <th>Quantity</th>
+                <th style="font-colour:blue">Product</th>
+                <th>Quantity</th>
+                <td>Price</th>
             </tr>
+
             <tr>
                 <td>Bag</td>
                 <td>50</td>
-                
+                <td><button type="submit" value="Submit">Quantity</button></td>
             </tr>
         </table>
+        <button type="submit" value="Submit">Checkout</button>
+
+        
+    <button type="button" onClick="click()">Click me</button>
+    <p>Clicks: <a id="clicks">0</a></p>
         </html>
+
+
 
     )
 
