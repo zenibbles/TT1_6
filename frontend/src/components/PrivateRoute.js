16157e6b93d1af07f534ec6227b1c6a48a7b9cf0
Han <hzhu009@e.ntu.edu.sg>
2021-09-30 11:28:30 +0800

    Create PrivateRoute.js

---
+++ frontend/src/components/PrivateRoute.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+
+const isLoggedIn = () => {
+    return localStorage.getItem('LoginTrigger');
+};
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+    <Route
+        {...rest}
+        render={(props) =>
+            isLoggedIn() ? (
+                <Component {...props} />
+            ) : (
+                <Redirect
+                    to={{
+                        pathname: '/login',
+                        state: { from: props.location },
+                    }}
+                />
+            )
+        }
+    ></Route>
+);
+
+export default PrivateRoute;
