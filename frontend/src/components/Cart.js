11c663be92e63ff52796cddb83f42768f2c5f7b4
lesterbek <lesterbek@gmail.com>
2021-09-30 13:32:09 +0800

    test

--- frontend/src/components/Cart.js
+++ frontend/src/components/Cart.js
@@ -1,7 +1,22 @@
 import React, {Component} from 'react';
 
 export default function Cart(){
+
     return(
+        <html>
         <h1>Cart Page</h1>
+        <table>
+            <th>Product</th>
+            <th>Quantity</th>
+            <tr>
+                <td>Bag</td>
+                <td>50</td>
+            </tr>
+        </table>
+        </html>
+
     )
+
+
+
 }
