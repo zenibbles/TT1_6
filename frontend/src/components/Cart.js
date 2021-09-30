ae0dacaba3537a0f4722dd5aba1c420ef270cbeb
lesterbek <lesterbek@gmail.com>
2021-09-30 13:49:56 +0800

    test

--- frontend/src/components/Cart.js
+++ frontend/src/components/Cart.js
@@ -7,11 +7,14 @@ export default function Cart(){
         <html>
         <h1>Cart Page</h1>
         <table>
+            <tr>
             <th>Product</th>
             <th>Quantity</th>
+            </tr>
             <tr>
                 <td>Bag</td>
                 <td>50</td>
+                
             </tr>
         </table>
         </html>
