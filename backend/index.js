de8a39c31064b7e1924ce944455c0b113a2359d3
zenibbles <65108129+zenibbles@users.noreply.github.com>
2021-09-30 14:42:26 +0800

    -

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,13 @@
 const http = require("http");
 const app = require("./app");
+const cors = require("cors")
+const bodyParser = require("body-parser");
 const server = http.createServer(app);
 const controller = require('./controllers/auth.controller')
 
 const { API_PORT } = process.env;
 const port = process.env.PORT || API_PORT;
 
-<<<<<<< Updated upstream
-=======
 var corsOptions = {
   origin: "http://localhost:5000"
 };
@@ -26,10 +26,9 @@ app.get("/", (req, res) => {
 });
 
 app.get("/api/auth/x", (req,res) => {
-  controller.login;
+  controller.login
 })
 
->>>>>>> Stashed changes
 server.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
