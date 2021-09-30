bbbb98c2ae8368fe77be64dfea0544ac4e461b48
Han <hzhu009@e.ntu.edu.sg>
2021-09-30 12:26:09 +0800

    Update Login Page

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,7 +1,71 @@
-import React, {Component} from 'react';
+import React, {Component, useEffect, useState } from 'react';
+import {useHistory} from "react-router-dom";
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import axios from 'axios';
 
 export default function Login(){
+    const [username, setUser] = useState("Enter Your Username Here");
+    const [password, setPassword] = useState("");
+    const [status,setStatus] = useState(localStorage.getItem('accessToken'));
+    let history = useHistory();
+
+    const handleSubmit = async(event) =>{
+        event.preventDefault()
+
+        try{
+            const response = await axios.post()
+            if(response.status == 200){
+                localStorage.setItem('accessToken',true);
+                localStorage.setItem('first_name',response.first_name);
+                localStorage.setItem('first_name',response.first_name);
+
+                history.push('productdetails');
+
+
+            }
+        }
+        catch(error){
+            alert(error.message);
+        }
+    }
+
+    const updatePassword = (event) => {
+        setPassword(event.target.value);
+    }
+    const updateUser = (event) => {
+        setUser(event.target.value);
+    }
+
+
     return(
+    <div className={"Login"}>
         <h1>Login Page</h1>
+        <Form onSubmit={handleSubmit}>
+            <Form.Group size={"lg"}>
+                <Form.Label>Username</Form.Label>
+                <Form.Control
+                    autoFocus type="text"
+                    size="lg"
+                    value={username}
+                    onChange={updateUser}
+                    required
+                />
+            </Form.Group>
+
+            <Form.Group size="lg">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                    autoFocus type="password"
+                    value={password}
+                    size="lg"
+                    onChange={updatePassword}
+                    required
+                />
+            </Form.Group>
+            <Button block size="lg" type="submit">Login</Button>
+        </Form>
+
+    </div>
     )
 }
